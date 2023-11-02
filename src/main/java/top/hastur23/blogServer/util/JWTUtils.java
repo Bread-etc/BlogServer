2d13b36a64acb2b67b2b887fcc6c7149f5158e80
Bread-etc <1054115624@qq.com>
2023-11-02 22:49:35 +0800

    改进JWT

--- src/main/java/top/hastur23/blogServer/util/JWTUtils.java
+++ src/main/java/top/hastur23/blogServer/util/JWTUtils.java
@@ -1,7 +1,10 @@
 package top.hastur23.blogServer.util;
 
+import cn.hutool.jwt.JWT;
+import org.springframework.util.StringUtils;
 import top.hastur23.blogServer.entity.User;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +23,7 @@ public class JWTUtils {
     * @return token
     * */
     public static String generateTokenForUser(User userInfo) {
-        Map<String, String> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("username", userInfo.getUsername());
         return generateToken(map);
     }
@@ -32,7 +35,41 @@ public class JWTUtils {
      * @param map
      * @return token
      * */
-    public static String generateToken(Map<String, String> map) {
-
+    public static String generateToken(Map<String, Object> map) {
+        JWT jwt = JWT.create();
+        // 设置携带参数
+        map.forEach(jwt::setPayload);
+        // 设置密钥
+        jwt.setKey(SECRET_KEY);
+        // 设置过期时间
+        jwt.setExpiresAt(new Date(System.currentTimeMillis() + EXPIRE * 1000));
+        return jwt.sign();
     }
+
+    /**
+     * token 校验
+     *
+     * @param token
+     * @return 是否通过校验
+     * */
+    public static boolean verify (String token) {
+        if (StringUtils.isBlank(token)) return false;
+        return JWT.of(token).setKey(SECRET_KEY).verify();
+    }
+
+    /**
+     * token 校验并且获取 userInfo
+     *
+     * @param token
+     * @return userInfo
+     * */
+    public static User verifyAndGetUser(String token) {
+        if (!verify(token)) return null;
+        // 解析数据
+        JWT jwt = JWT.of(token);
+        String username = jwt.getPayload("username").toString();
+        // 返回用户信息
+        return new User(username);
+    }
+
 }
